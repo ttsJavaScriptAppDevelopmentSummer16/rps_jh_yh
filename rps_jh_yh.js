9e3e781deeafa6bad55db787dad065e125060029
Jude Harding <jharding@genesisgroupusa.com>
2016-07-20 22:12:58 -0400

    working on ifs

--- rps_jh_yh.js
+++ rps_jh_yh.js
@@ -12,8 +12,8 @@ var computerHand = weapons[computer];
 
 var handCount = 1;
 
-while (handCount < 6) {
-// while(playerScore < 5 || computerScore < 5) {
+
+while(playerScore < 4 || computerScore < 4) {
     console.log("in the WHILE -- Hand Count " + handCount);
 
     var computer = parseInt(Math.random()*10)%3;
@@ -102,113 +102,3 @@ while (handCount < 6) {
 //     console.log("COMPUTER picked scissors");
 //     break;
 //   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var playerName = prompt("Enter name");
-// var playerChoice = prompt("What do you choose, rock, paper or scissors?");
-//
-//
-// var rock = 0;
-// var paper = 1;
-// var scissors = 2;
-//
-// var computer;
-// var computerScore = 0;
-//
-// switch(playerChoice){
-//   case 'rock':
-//     console.log("You picked " + playerChoice);
-//     break;
-//   case 'paper':
-//     console.log("You picked " + playerChoice);
-//     break;
-//   case 'scissors':
-//     console.log("You picked " + playerChoice);
-//     break;
-//   default:
-//     console.log("You automatically lose");
-// }
-//
-// computer = parseInt(Math.random()*10)%3;
-// console.log (computer);
-//
-// switch(computer){
-//   case 0:
-//     console.log("Computer picked rock");
-//     break;
-//   case 1:
-//     console.log("Computer picked paper");
-//     break;
-//   case 2:
-//     console.log("Computer picked scissors");
-//     break;
-//   }
-//
-// if (computer == 0) {
-//   switch(playerChoice){
-//     case 'rock':
-//       console.log("It's a tie");
-//       break;
-//     case 'paper':
-//       console.log("You win");
-//       playerScore = playerScore + 1;
-//       break;
-//     case 'scissors':
-//       console.log("You lose");
-//       break;
-//   }
-// }
-//
-// if (computer == 1) {
-//   switch(playerChoice){
-//     case 'rock':
-//       console.log("You lose");
-//       break;
-//     case 'paper':
-//       console.log("It's a tie");
-//       break;
-//     case 'scissors':
-//       console.log("You win");
-//       playerScore = playerScore + 1;
-//       break;
-//   }
-// }
-//
-// if (computer == 2) {
-//   switch(playerChoice){
-//     case 'rock':
-//       console.log("You win");
-//       playerScore = playerScore + 1;
-//       break;
-//     case 'paper':
-//       console.log("You lose");
-//       break;
-//     case 'scissors':
-//       console.log("It's a tie");
-//       break;
-//   }
-// }
-// console.log (playerName + "\'s score is " + playerScore);
-//
-// }
